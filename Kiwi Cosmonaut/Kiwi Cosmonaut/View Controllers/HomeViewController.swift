552fbc42b6ffa6a6c813223b6a3ed25bed70b17e
TimMarotta <timothy.marotta@yahoo.com>
2021-04-15 14:29:14 -0400

    migrated views to main.storyboard and added connection

---
+++ Kiwi Cosmonaut/Kiwi Cosmonaut/View Controllers/HomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.swift
+//  Kiwi Cosmonaut
+//
+//  Created by Timothy Marotta on 4/14/21.
+//
+
+import Foundation
+import SwiftUI
+
+class HomeViewController: UIViewController {
+    
+}
