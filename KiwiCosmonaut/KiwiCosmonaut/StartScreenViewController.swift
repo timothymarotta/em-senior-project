cfb3461c84cf7f2730a0866e48763e6392fa472d
TimMarotta <timothy.marotta@yahoo.com>
2021-04-20 17:17:25 -0400

    initial commit; start view

---
+++ KiwiCosmonaut/KiwiCosmonaut/StartScreenViewController.swift
@@ -0,0 +1,13 @@
+//
+//  StartScreenViewController.swift
+//  KiwiCosmonaut
+//
+//  Created by Timothy Marotta on 4/20/21.
+//
+
+import Foundation
+import UIKit
+
+class StartScreenViewController: UIViewController {
+    
+}
