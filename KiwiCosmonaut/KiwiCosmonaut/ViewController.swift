8fd29a16b7b14c1457cbf0a92dff4c4fec34307e
TimMarotta <timothy.marotta@yahoo.com>
2021-04-20 15:32:56 -0400

    project researt; SwiftUI version in deprecated, working copy in root directory

---
+++ KiwiCosmonaut/KiwiCosmonaut/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  KiwiCosmonaut
+//
+//  Created by Timothy Marotta on 4/20/21.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
--- deprecated/Kiwi Cosmonaut/Kiwi Cosmonaut/View Controllers/HomeViewController.swift
+++ deprecated/Kiwi Cosmonaut/Kiwi Cosmonaut/View Controllers/HomeViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import SwiftUI
+import UIKit
 
 class HomeViewController: UIViewController {
     
