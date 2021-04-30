6c53e2fed66b2b5e7fa9c58a660e8bde838febb1
cfiore2356 <60017905+cfiore2356@users.noreply.github.com>
2021-04-29 21:20:39 -0400

    level build and anchor entity
    level Zero is built in reality composer and code has been written to create a anchor entity and load a experience entity into the scene

--- KiwiCosmonaut/KiwiCosmonaut/ViewController.swift
+++ KiwiCosmonaut/KiwiCosmonaut/ViewController.swift
@@ -1,24 +1,91 @@
 //
 //  ViewController.swift
-//  KiwiCosmonaut
+//  Kiwi Cosmonaut
 //
-//  Created by Timothy Marotta on 4/20/21.
+//  Created by Catherine Fiore on 4/19/21.
 //
 
+import Foundation
 import UIKit
 import RealityKit
+import ARKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController{
     
     @IBOutlet var arView: ARView!
+   
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    //var levelAnchor: Experience.LevelZero?
+    
+    //can also be viewDidAppear
+    override func viewDidAppear(_ animated: Bool){
+            super.viewDidAppear(animated)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+            
+            arView.session.delegate = self
+            setupARView()
+            
+            arView.addGestureRecognizer(UITapGestureRecognizer(target:self, action: #selector(handleTap(recognizer:))))
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        //this was just the built in code
+            //let levelAnchor = try! Experience.loadLevelZero()
+            //arView.scene.anchors.append(levelAnchor)
+            //self.levelAnchor = levelAnchor
+        
+    }
+    
+    //MARK: Set up Methods
+    
+    func setupARView(){
+        //overrides the automatic set up
+        arView.automaticallyConfigureSession = false
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = [.horizontal, .vertical]
+        configuration.environmentTexturing =  .automatic
+        arView.session.run(configuration)
+    }
+    
+    //MARK: Object Placement
+    @objc func handleTap (recognizer: UITapGestureRecognizer){
+        let location = recognizer.location(in: arView)
+        
+
+        let results = arView.raycast(from:location, allowing: .estimatedPlane, alignment: .horizontal)
+        
+        if let firstResult = results.first{
+            let anchor = ARAnchor (name: "beacon", transform: firstResult.worldTransform)
+            arView.session.add(anchor: anchor)
+        } else{
+            print("Object placement failed. Couldnt find surface.")
+        }
+    }
+    
+    func placeObject(named entityName:String, for anchor: ARAnchor){
+        
+        //let entity = try! ModelEntity.loadModel(named: entityName)
+        let entity = try! Experience.loadLevelZero()
+    
+        //allows you to move it into the scene
+        entity.generateCollisionShapes(recursive: true)
+        //gesture control for rotation
+        arView.installGestures([.rotation,.translation], for: entity as! HasCollision)
+        
+        //creates a enchor entity with the location we created earlier
+        let anchorEntity = AnchorEntity(anchor: anchor)
+        anchorEntity.addChild(entity)
+        arView.scene.addAnchor(anchorEntity)
+    }
+    
+}
+
+extension ViewController: ARSessionDelegate {
+    public func session(_ session:ARSession, didAdd anchors: [ARAnchor]){
+        for anchor in anchors {
+            if let anchorName = anchor.name, anchorName == "beacon" {
+                placeObject(named: anchorName, for:anchor)
+            }
+        }
     }
 }
+
