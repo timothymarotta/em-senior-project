ca916c5bff3c55cc0160194f3dd45e0d542ccf33
cfiore2356 <60017905+cfiore2356@users.noreply.github.com>
2021-05-04 22:29:45 -0400

    Music and Levels
    Added music to the home screen and fleshed out the levels working on some game testing

--- KiwiCosmonaut/KiwiCosmonaut/LevelScreenViewController.swift
+++ KiwiCosmonaut/KiwiCosmonaut/LevelScreenViewController.swift
@@ -7,7 +7,29 @@
 
 import Foundation
 import UIKit
+//import AVFoundation
 
 class LevelScreenViewController: UIViewController {
+    
+    //var player:AVAudioPlayer = AVAudioPlayer()
+    
+    
+
+  //  override func viewDidLoad() {
+  //      super.viewDidLoad()
+        
+//        do
+//        {
+//            let audioPath = Bundle.main.path(forResource: "maybe", ofType: "mp3")
+//            try player = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: audioPath!) as URL)
+//        }
+//        catch{
+//            //ERROR
+//        }
+//        player.play()
+//        player.numberOfLoops = -1
+//    }
+    
+    
     
 }
