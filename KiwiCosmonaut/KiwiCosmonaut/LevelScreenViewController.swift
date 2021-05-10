09cc6deca1b07e9156329cbccce01961441c08d2
cfiore2356 <60017905+cfiore2356@users.noreply.github.com>
2021-05-10 09:58:53 -0400

    Added Video
    Button on level screen nows allows the video to be played

--- KiwiCosmonaut/KiwiCosmonaut/LevelScreenViewController.swift
+++ KiwiCosmonaut/KiwiCosmonaut/LevelScreenViewController.swift
@@ -12,29 +12,37 @@ import AVKit
 
 class LevelScreenViewController: UIViewController {
     
-//
-//    let vc = AVPlayerViewController()
-//
-//     override func viewDidLoad() {
-//            super.viewDidLoad()
-//
-//
-//        }
-//
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "kiwiCosmoFinalCopy", ofType: "mp4")!))
-//        vc.player = player
-//        NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: vc.player?.currentItem)
-//        present(vc, animated: true)
-//
-//    }
-//
-//    @objc func playerDidFinishPlaying(note: NSNotification) {
-//           self.vc.dismiss(animated: true)
-//    }
+  
+    let vc = AVPlayerViewController()
     
-}
+    override func viewDidLoad() {
+            super.viewDidLoad()
+        
+        }
+    
+    override func didReceiveMemoryWarning() {
+         super.didReceiveMemoryWarning()
+     }
+
+
+    @IBAction func playVideo(_ sender: Any) {
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "kiwiCosmoFinalCopy", ofType: "mp4")!))
+
+        vc.player = player
+        NotificationCenter.default.addObserver(self, selector: #selector(playerDidFinishPlaying), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: vc.player?.currentItem)
+
+        self.present(vc, animated: true) {
+            self.vc.player!.play()
+              }
+
+    }
+
+    @objc func playerDidFinishPlaying(note: NSNotification) {
+          self.vc.dismiss(animated: true)
+        //super.viewDidLoad()
+      }
+  }
+    
 
 
 
